fd60e1de04cbf7ce583b1624797739b868804473
Idris Westbrook <westbrook.idris@gmail.com>
2022-10-31 00:05:42 -0400

    add header and form components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import './App.css'
+import Header from './components/Header'
+import Meme from './components/Meme'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <Header />
+      <Meme />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="./images" className="header-image" />
+            <h2 className="header-title">Meme Generator</h2>
+        </header>
+    )
+}
