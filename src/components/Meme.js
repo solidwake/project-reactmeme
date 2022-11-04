c161c44aa513c87cd7b10ec63046972af00d7c39
Idris Westbrook <westbrook.idris@gmail.com>
2022-11-04 09:36:39 -0400

    edit eslintrc

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,17 @@
+import memesData from '../memesData'
+
 export default function Meme() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    console.log(randomNumber)
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form-input" placeholder="Top text" />
                 <input type="text" className="form-input" placeholder="Bottom text" />
                 <button className="form-button">Get a new meme image</button>
-            </form>
+            </div>
         </main>
     )
 }
